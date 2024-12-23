d193443cc26bafa253e7a273bf3c2a5b02b900a3
arpit mishra <arpit.mishra.out@gmail.com>
2024-12-23 22:14:34 +0530

    Introduction To NodeJS and History of Node.JS

---
+++ NamasteyNode/episode01.js
@@ -0,0 +1,18 @@
+`use strict`;
+
+// Introduction To NodeJS
+// Nodejs is a javascript runtime built on chrome's v8 Javascript engine.
+// Node.js is a cross-platform, open-source Javascript runtime environment.
+// Node.js is maintained by the community called openjs foundation.
+// Node.js has an event-driven architecture capable of asynchronous I/O.
+// Asynchronous I/O is also known as non-blocking I/O.
+
+// History of Node.js
+// Ryan Dahl developed Node.js in 2009.
+// To run, JS code you need a JS engine.
+// When Ryan first started developing Node.js, he does not uses chrome's v8 JS enging instead uses firefox 's spidermonkey JS engine.
+// Initially Node was names web.js by Ryan. later named it to Node.js due to it's huge potential.
+// Ryan wanted to create a non-blocking server which helps in handling many requests in less threads.
+// In 2010,NPM Happened which is a package manager for Node.js.
+// Initially Node.js was for macos and linux but in 2011 with Joyent and Microsoft support , it was made ofor windows.
+// In 2019 Open JS foundation takes control of Node.js
