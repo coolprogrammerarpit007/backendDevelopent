fecd0919624ca1e630a442124ad79b735e20905b
arpit mishra <arpit.mishra.out@gmail.com>
2024-12-24 22:17:03 +0530

    Intro to Server

---
+++ NamasteyNode/episode02.js
@@ -0,0 +1,5 @@
+`use strict`;
+
+// Javascript on Server
+// Whenever a client  serves a request to the server, that domain is connected to an IP Address. that IP Addesss points to a server.
+// Server is also a computer which serves client by serving to client's request by providing resources.
