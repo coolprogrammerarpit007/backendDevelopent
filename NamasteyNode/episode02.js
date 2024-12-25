06116993d9b4f5c5a84517f8e39de062f030c044
arpit mishra <arpit.mishra.out@gmail.com>
2024-12-25 23:16:48 +0530

    JS On Server

--- NamasteyNode/episode02.js
+++ NamasteyNode/episode02.js
@@ -3,3 +3,8 @@
 // Javascript on Server
 // Whenever a client  serves a request to the server, that domain is connected to an IP Address. that IP Addesss points to a server.
 // Server is also a computer which serves client by serving to client's request by providing resources.
+// The job of v8 engine is to execute JS code.
+// Node .js is a c++ application with v8 embedded on it.
+// Ecmascript is a standard for scripting languages including javascript, jsscript and actionscript.
+// JS engines need to follow these EcmaScript Standards.
+// whenever we wrote JS code, JS engine (written in c++) converts that JS code (high level code) to low-level code(Machine code) which is executed by the system. and run our JS application outside browser.
